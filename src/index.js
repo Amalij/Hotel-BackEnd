aae363299a545b7049e35e9b91cf0fbf367b4c76
Amali <ishankaamali975@gmail.com>
2025-03-01 21:10:55 +0530

    Ignore .env file

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import connectDB from "./infrastructure/db.js";
+
+import hotelsRouter from "./api/hotel.js";
+import usersRouter from "./api/user.js";
+import bookingsRouter from "./api/booking.js";
+
+// Create an Express instance
+const app = express();
+// Middleware to parse JSON data in the request body
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/hotels", hotelsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/bookings", bookingsRouter);
+
+// Define the port to run the server
+const PORT = 8000;
+app.listen(PORT, console.log(`Server is running on port ${PORT}...`));
